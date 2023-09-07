ba62276ff26ed0bb49cbd690815153458841a857
Ankita-Beck <anneankita8@gmail.com>
2023-09-07 11:53:57 +0530

    Printing name and surname

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-print("HELLO WORLD")
-print(8)
-
+# print("HELLO WORLD")
+# print(8)
+#
 name = "Ankita"
 surname = "Beck"
 age = 22
-
+#
 print(name)
 print(surname)
-print(age)
+# print(age)
+print("My first name is {} and my last name is {}".format(name,surname))
+
